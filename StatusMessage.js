e43c15f652442317448d16dd2a286b613ad0dcf1
Nathan Mattick <nathan.mattick@petards.com>
2022-02-15 07:21:33 +0000

    Start of timers

--- StatusMessage.js
+++ StatusMessage.js
@@ -1,36 +1,37 @@
-var Dequeue = require('dequeue')
-
 class Status_Message {
-    constructor(application, message) {
-        this.application = application;
-        this.message = message;
-        Status_Message.statuses.push(this);
-    }
+  constructor(application, message) {
+    this.application = application;
+    this.message = message;
 
-    get_application() {
-        return this.application;
-    }
+    var o = {};
+    o['application'] = application;
+    o['message'] = message;
+    // console.log(JSON.stringify(o));
+    Status_Message.statuses.push(JSON.stringify(o));
+  }
 }
 
-Status_Message.print_sm = function() {
-    var i = 0;
-    while (i < Status_Message.statuses.length) {
-        var sm = Status_Message.statuses[i];
-        console.log(sm.get_application());
-        i += 1;
-    }
-}
+Status_Message.print_sm = function () {
+  for (const s of Status_Message.statuses) {
+    console.log(s);
+  }
+};
 
-Status_Message.statuses = new Dequeue();
+Status_Message.statuses = [];
 
 function main() {
-    console.log("Hello world");
-    sm = new Status_Message('config', 'true');
-  }
-  
-  if (require.main === module) {
-    main();
-    Status_Message.print_sm();
+  console.log("## status messaages ##");
+  sm = new Status_Message("config", "true");
 
-  }
+  for (i = 0 ; i < 5 ; i++) {
+    new Status_Message("app_" + i.toString(), "msg_" + i.toString());
+  }   
 
+  // https://nodejs.org/en/docs/guides/timers-in-node/
+  t = setInterval(Status_Message.print_sm, 1500);
+}
+
+if (require.main === module) {
+  main();
+  Status_Message.print_sm();
+}
